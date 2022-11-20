afa8bd15eea4bbbcde6922373cf344519478d5a7
Buwenjia <113284463+Buwenjia@users.noreply.github.com>
2022-11-20 17:25:17 +0800

    Update PhaSpaRecon.m

--- differential phase diagram/PhaSpaRecon.m
+++ differential phase diagram/PhaSpaRecon.m
@@ -1,12 +1,4 @@
 function [xn,dn] = PhaSpaRecon(s,tau,m)
-% 混沌序列的相空间重构 (phase space reconstruction)
-% [xn, dn, xn_cols] = PhaSpaRecon(s, tau, m)
-% 输入参数：    s          混沌序列(列向量)
-%               tau        重构时延
-%               m          重构维数
-% 输出参数：    xn         相空间中的点序列(每一列为一个点)
-%               dn         一步预测的目标(行向量)
-
 [rows,cols] = size(s);
 if (rows>cols)
     len = rows;
@@ -24,9 +16,9 @@ if (nargout==2)
     else
         xn = zeros(m,len-1-(m-1)*tau);
         for i = 1:m
-            xn(i,:) = s(1+(i-1)*tau : len-1-(m-i)*tau);   % 相空间重构，每一行为一个点 
+            xn(i,:) = s(1+(i-1)*tau : len-1-(m-i)*tau);  
         end
-        dn = s(2+(m-1)*tau : end);    % 预测的目标
+        dn = s(2+(m-1)*tau : end);   
     end
     
 elseif (nargout==1)
@@ -37,11 +29,9 @@ elseif (nargout==1)
     else
         xn = zeros(m,len-(m-1)*tau);
         for i = 1:m
-            xn(i,:) = s(1+(i-1)*tau : len-(m-i)*tau);   % 相空间重构，每一行为一个点 
+            xn(i,:) = s(1+(i-1)*tau : len-(m-i)*tau);  
         end
     end
     
 end
-
-
 
